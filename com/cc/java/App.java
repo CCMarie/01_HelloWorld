8bf816bf851df66f6f27478fac879ad63051425d
Maria <sydowmarie4@gmail.com>
2024-01-24 09:35:47 +0100

    New 2

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,11 +1,32 @@
 package com.cc.java;
 
-public class App 
-{
+public class App {
 
    public static void main(String[] args) {
-    System.out.println("Hello, World!");
-   }
+
+    
+
+      Cat cat = new Cat();
+
+      System.out.println(cat);
+
+      cat.sayHello();
+
+
+
+
+    }
+
+      
+
+
+
+
+
+
+   
+
+  
 
 
 
---
+++ com/cc/java/Cat.java
@@ -0,0 +1,18 @@
+package com.cc.java;
+
+
+
+
+public class Cat {
+   
+public void sayHello(){
+
+    System.out.println("Hello");
+
+}
+
+
+
+}
+
+
